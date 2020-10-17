99bdf4bad11c3edbc5c4f908fa84f9eee99902ba
filipkrzyz <krzyzanowski.f@gmail.com>
2020-10-17 16:40:25 +0200

    98 Show Tweets from Followed Users only. Add Refresh control to the Feed. Sort Tweets by date. Fixing Liked tweets bug

--- TwitterTutorial/API/TweetService.swift
+++ TwitterTutorial/API/TweetService.swift
@@ -40,14 +40,25 @@ struct TweetService {
     
     func fetchTweets(completion: @escaping([Tweet]) -> Void) {
         var tweets = [Tweet]()
+        guard let currentUid = Auth.auth().currentUser?.uid else { return }
         
-        REF_TWEETS.observe(.childAdded) { snapshot in
-            guard let dictionary = snapshot.value as? [String: Any] else { return }
-            guard let uid = dictionary["uid"] as? String else { return }
+        REF_USER_FOLLOWING.child(currentUid).observe(.childAdded) { snapshot in
+            let followingUid = snapshot.key
             
-            UserService.shared.fetchUser(uid: uid) { (user) in
+            REF_USER_TWEETS.child(followingUid).observe(.childAdded) { snapshot in
                 let tweetID = snapshot.key
-                let tweet = Tweet(user: user, tweetID: tweetID, dictionary: dictionary)
+                
+                self.fetchTweet(withTweetID: tweetID) { tweet in
+                    tweets.append(tweet)
+                    completion(tweets)
+                }
+            }
+        }
+        
+        REF_USER_TWEETS.child(currentUid).observe(.childAdded) { snapshot in
+            let tweetID = snapshot.key
+            
+            self.fetchTweet(withTweetID: tweetID) { tweet in
                 tweets.append(tweet)
                 completion(tweets)
             }
--- TwitterTutorial/Controller/FeedController.swift
+++ TwitterTutorial/Controller/FeedController.swift
@@ -47,17 +47,22 @@ class FeedController: UICollectionViewController {
     // MARK: - API
     
     func fetchTweets() {
+        collectionView?.refreshControl?.beginRefreshing()
         TweetService.shared.fetchTweets { tweets in
-            self.tweets = tweets
-            self.checkIfTweetsAreLiked(tweets)
+            self.tweets = tweets.sorted(by: { $0.timestamp > $1.timestamp })
+            self.checkIfTweetsAreLiked()
+            
+            self.collectionView?.refreshControl?.endRefreshing()
         }
     }
     
-    func checkIfTweetsAreLiked(_ tweets: [Tweet]) {
-        for (index, tweet) in tweets.enumerated() {
+    func checkIfTweetsAreLiked() {
+        self.tweets.forEach { tweet in
             TweetService.shared.checkIfTweetIsLiked(tweet: tweet) { isLiked in
                 guard isLiked == true else { return }
-                self.tweets[index].isLiked = true
+                if let index = self.tweets.firstIndex(where: { $0.tweetID == tweet.tweetID }) {
+                    self.tweets[index].isLiked = true
+                }
             }
         }
     }
@@ -74,6 +79,10 @@ class FeedController: UICollectionViewController {
         imageView.contentMode = .scaleAspectFit
         imageView.setDimensions(width: 44, height: 44)
         navigationItem.titleView = imageView
+        
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
+        collectionView.refreshControl = refreshControl
     }
     
     func configureLeftBarButton() {
@@ -87,6 +96,12 @@ class FeedController: UICollectionViewController {
         profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: profileImageView)
+    }
+    
+    // MARK: - Selectors
+    
+    @objc func handleRefresh() {
+        fetchTweets()
     }
 }
 
